20b86ece4ddadf79df184fc0126832f43be4a78e
Shridhar-Achari <shridharachar2706@gmail.com>
2022-04-02 17:43:14 +0530

    commit

---
+++ Task11/JS/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    $("#jbtn1").click(function(){
+        $("#jobj").hide();
+    });
+});
+
+
+$(document).ready(function(){
+    $("#jbtn2").click(function(){
+        $("#jclass").hide();
+    });
+});
+
+
+$(document).ready(function(){
+    $("#jbtn3").click(function(){
+        $("#jproto").hide();
+    });
+});
+
+
+$(document).ready(function(){
+    $("#jbtn4").click(function(){
+        $("#jabstract").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#jbtn5").click(function(){
+        $("#jencap").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#jbtn6").click(function(){
+        $("#jpoly").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#jbtn7").click(function(){
+        $("#jinherit").hide();
+    });
+});
+
+
+
